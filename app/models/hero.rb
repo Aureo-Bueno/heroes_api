22355441dce22e6bb60240951dd8be116e74e3d3
Aure-Bueno <aureoalexandre@outlook.com>
2023-06-07 19:18:11 -0300

    feat: add scope index controoler

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,6 @@
 class Hero < ApplicationRecord
   validates :name, presence: true
 
+  scope :search, -> (term) { where('LOWER(name) LIKE ?', "%#{term.document}%") if term.present? }
   scope :sorted_by_name, -> { order(:name) }
 end
